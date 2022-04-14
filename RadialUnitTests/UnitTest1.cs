acdc6d1fd3e3e0f518fc85676a5b7b53df31d3f1
brajm008 <ujkkju117@outlook.com>
2022-04-14 22:39:08 +0930

    add test proj

---
+++ RadialUnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+
+namespace RadialUnitTests
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData("","","")]
+        [InlineData(null,null,null)]
+        public void Test1(string s1, string s2, string s3)
+        {
+            
+        }
+    }
+}
