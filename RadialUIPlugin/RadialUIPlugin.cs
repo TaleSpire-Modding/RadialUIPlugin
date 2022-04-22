f3b26a92abced7b7cdeab9a1fd3ffbfec184b78f
brajm008 <ujkkju117@outlook.com>
2022-04-22 18:49:41 +0930

    First load of unit testing.

--- RadialUIPlugin/RadialUIPlugin.cs
+++ RadialUIPlugin/RadialUIPlugin.cs
@@ -31,7 +31,7 @@ namespace RadialUI
         /// <param name="key">The plugin's GUID or key</param>
         /// <param name="value">The specific value being cleared from dictionary's list.</param>
 		/// <param name="shouldRemoveCallback">the callback to call upon checking</param>
-		internal static void AddRemoveOn(Dictionary<string, List<RadialCheckRemove>> data, string key, string value, ShouldShowMenu shouldRemoveCallback)
+		public static void AddRemoveOn(Dictionary<string, List<RadialCheckRemove>> data, string key, string value, ShouldShowMenu shouldRemoveCallback)
 		{
 			if (!data.ContainsKey(key))
 				data.Add(key, new List<RadialCheckRemove>());
@@ -39,27 +39,28 @@ namespace RadialUI
 			data[key].Add(new RadialCheckRemove(value, shouldRemoveCallback));
 		}
 
-		/// <summary>
-		/// Default method callback for RadialCheckRemove.
-		/// Paramaters are to satisfy delete struct.
-		/// </summary>
-		/// <param name="s1">title text</param>
-		/// <param name="s2">mini id</param>
-		/// <param name="s3">target id</param>
-		/// <returns></returns>
-        internal static bool alwaysTrue(string s1, string s2, string s3)
+        /// <summary>
+        /// Default method callback for RadialCheckRemove.
+        /// Parameters are to satisfy delete struct.
+        /// </summary>
+        /// <param name="s1">title text</param>
+        /// <param name="s2">mini id</param>
+        /// <param name="s3">target id</param>
+        /// <returns>True</returns>
+        public static bool alwaysTrue(string s1, string s2, string s3)
         {
             return true;
         }
 
-		/// <summary>
+
+        /// <summary>
 		/// Removes a default hider on an existing dictionary.
 		/// </summary>
 		/// <param name="data">the dictionary storing the existing menus to hide.</param>
 		/// <param name="key">The plugin's GUID or key</param>
 		/// <param name="value">The specific value being cleared from dictionary's list.</param>
 		/// <returns></returns>
-        internal static bool RemoveRemoveOn(Dictionary<string, List<RadialCheckRemove>> data, string key, string value)
+        public static bool RemoveRemoveOn(Dictionary<string, List<RadialCheckRemove>> data, string key, string value)
 		{
 			if (!data.ContainsKey(key))
 				return false;
---
+++ RadialUnitTests/RadialUIPluginTests.cs
@@ -0,0 +1,104 @@
+using System.Collections.Generic;
+using System.Linq;
+using RadialUI;
+using Xunit;
+
+namespace RadialUnitTests
+{
+
+    // First simple test and asserts regardless of inputs, return true. Used for buttons on minis as default
+    public class RadialUIPluginTests
+    {
+        public static bool CustomShouldShowMenu(string menuText, string miniId, string targetId) => true;
+
+        [Theory]
+        [InlineData("","","")]
+        [InlineData(null,null,null)]
+        public void TestAlwaysTrue(string s1, string s2, string s3)
+        {
+            Assert.True(RadialUIPlugin.alwaysTrue(s1, s2, s3));
+        }
+
+        [Fact]
+        public void TestAddRemoveOnAddNewKeyIfDoesNotExist()
+        {
+            Dictionary<string, List<RadialCheckRemove>> data = new Dictionary<string, List<RadialCheckRemove>>();
+            string key = "my.Plugin.Key";
+            string value = "my.text.value";
+            
+            Assert.Empty(data);
+            Assert.DoesNotContain(key,data.Keys);
+
+            RadialUIPlugin.AddRemoveOn(data,key,value,CustomShouldShowMenu);
+
+            Assert.Contains(key, data.Keys);
+            Assert.True(data[key].SingleOrDefault( data => data.TitleToRemove == value) != null);
+            Assert.True(data[key].SingleOrDefault(data => data.TitleToRemove == value).ShouldRemoveCallback == CustomShouldShowMenu);
+        }
+
+        [Fact]
+        public void TestAddRemoveOnNull()
+        {
+            Dictionary<string, List<RadialCheckRemove>> data = new Dictionary<string, List<RadialCheckRemove>>();
+            string key = "my.Plugin.Key";
+            string value = "my.text.value";
+
+            Assert.Empty(data);
+            Assert.DoesNotContain(key, data.Keys);
+
+            RadialUIPlugin.AddRemoveOn(data, key, value, null);
+
+            Assert.Contains(key, data.Keys);
+            Assert.True(data[key].SingleOrDefault(data => data.TitleToRemove == value) != null);
+            Assert.True(data[key].SingleOrDefault(data => data.TitleToRemove == value).ShouldRemoveCallback == RadialUIPlugin.alwaysTrue);
+        }
+
+        [Fact]
+        public void TestRemoveRemoveOnPass()
+        {
+            Dictionary<string, List<RadialCheckRemove>> data = new Dictionary<string, List<RadialCheckRemove>>();
+            string key = "my.Plugin.Key";
+            string value = "my.text.value";
+
+            RadialUIPlugin.AddRemoveOn(data, key, value, null);
+
+            Assert.Contains(key, data.Keys);
+            Assert.True(data[key].SingleOrDefault(data => data.TitleToRemove == value) != null);
+
+            var result = RadialUIPlugin.RemoveRemoveOn(data, key, value);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void TestRemoveRemoveOnFailWithWrongKey()
+        {
+            Dictionary<string, List<RadialCheckRemove>> data = new Dictionary<string, List<RadialCheckRemove>>();
+            string key = "my.Plugin.Key";
+            string value = "my.text.value";
+
+            RadialUIPlugin.AddRemoveOn(data, key, value, null);
+
+            Assert.Contains(key, data.Keys);
+            Assert.True(data[key].SingleOrDefault(data => data.TitleToRemove == value) != null);
+
+            var result = RadialUIPlugin.RemoveRemoveOn(data, "wrong key", "Doesn't exist");
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TestRemoveRemoveOnFailWithWrongValue()
+        {
+            Dictionary<string, List<RadialCheckRemove>> data = new Dictionary<string, List<RadialCheckRemove>>();
+            string key = "my.Plugin.Key";
+            string value = "my.text.value";
+
+            RadialUIPlugin.AddRemoveOn(data, key, value, null);
+
+            Assert.Contains(key, data.Keys);
+            Assert.True(data[key].SingleOrDefault(data => data.TitleToRemove == value) != null);
+
+            var result = RadialUIPlugin.RemoveRemoveOn(data, key, "Doesn't exist");
+            Assert.False(result);
+        }
+    }
+}
